a6d894a938fd627d453573a9e71159845c466bf3
Mixxa02 <mihajlo.paunovici4@gmail.com>
2026-02-28 11:57:26 +0100

    finall

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/https://mixxa02.github.io/aurel.github.io/',
+  build: {outDir: 'dist'},
   server: {
     host: "::",
     port: 8080,
